81bdc6c71f2b435629e5f972ebc32afad76afee3
Jasper Ketelaar <jasperketelaar@kpnmail.nl>
2015-11-18 16:30:55 +0100

    Gebruiker toegevoegd; database update

--- src/nl/tudelft/twobrains/server/model/Database.java
+++ src/nl/tudelft/twobrains/server/model/Database.java
@@ -1,32 +1,34 @@
 package nl.tudelft.twobrains.server.model;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by jasperketelaar on 11/18/15.
  */
 public class Database {
 
-    public Database() {
+    private final ArrayList<Gebruiker> gebruikers;
 
+    public Database(final ArrayList<Gebruiker> gebruikers) {
+        this.gebruikers = gebruikers;
     }
 
     public static Database parse(final String file) throws FileNotFoundException {
         final JSONParser parser = new JSONParser();
         final FileReader reader = new FileReader(file);
+        final ArrayList<Gebruiker> gebruikers = new ArrayList<>();
         try {
             final JSONObject obj = (JSONObject) parser.parse(reader);
-            System.out.println(obj.get("kvanzeijl@hotmail.com"));
+            for (final Object key : obj.keySet()) {
+                gebruikers.add(Gebruiker.parse((JSONObject) obj.get(key), (String) key));
+            }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
--- src/nl/tudelft/twobrains/server/model/Gebruiker.java
+++ src/nl/tudelft/twobrains/server/model/Gebruiker.java
@@ -1,7 +1,39 @@
 package nl.tudelft.twobrains.server.model;
 
+import org.json.simple.JSONObject;
+
 /**
  * Created by jasperketelaar on 11/18/15.
  */
 public class Gebruiker {
+
+    private final String voornaam;
+    private final String achternaam;
+    private final String email;
+    private final String wacthwoord;
+    private final String leeftijd;
+    private final String geslacht;
+    private final String foto;
+    private final String locatie;
+    private final String[] vakken;
+    private final String opleiding;
+
+    public Gebruiker(final String voornaam, final String achternaam, final String email, final String wachtwoord, final String leeftijd,
+                     final String geslacht, final String foto, final String locatie, final String[] vakken, final String opleiding) {
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+        this.email = email;
+        this.wacthwoord = wachtwoord;
+        this.leeftijd = leeftijd;
+        this.geslacht = geslacht;
+        this.foto = foto;
+        this.locatie = locatie;
+        this.vakken = vakken;
+        this.opleiding = opleiding;
+    }
+
+    //TODO: Write implementation
+    public static Gebruiker parse(final JSONObject object, final String email;) {
+        return null;
+    }
 }
