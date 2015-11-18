5a247ca719dcb864e7417266f23361ecba2619ac
lvelzel <leroyvelzel@hotmail.com>
2015-11-18 14:03:06 +0100

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//Test
+//Test2
 public class Main extends Application {
 
     @Override
