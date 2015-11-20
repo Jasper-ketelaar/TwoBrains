7d675963ed877ea0dc44300df590f7c70fea43bb
koenvzeijl <kvanzeijl@hotmail.com>
2015-11-20 09:52:05 +0100

    testClass voor gebruiker
    testen moeten wel nog geschreven worden

---
+++ src/nl/tudelft/twobrains/server/model/GebruikerTest.groovy
@@ -0,0 +1,19 @@
+package src.nl.tudelft.twobrains.server.model
+
+class GebruikerTest extends groovy.util.GroovyTestCase {
+    void testGetEmail() {
+
+    }
+
+    void testGetVoornaam() {
+
+    }
+
+    void testGetAchternaam() {
+
+    }
+
+    void testGetWachtwoord() {
+
+    }
+}
