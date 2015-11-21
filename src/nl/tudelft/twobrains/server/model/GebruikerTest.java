c4a434300a51e69b4e3de66106453833f025306f
koenvzeijl <kvanzeijl@hotmail.com>
2015-11-21 12:45:59 +0100

    Kijken of dit werkt

---
+++ src/nl/tudelft/twobrains/server/model/GebruikerTest.java
@@ -0,0 +1,39 @@
+package nl.tudelft.twobrains.server.model;
+
+import junit.framework.TestCase;
+import org.json.simple.JSONObject;
+import org.junit.Test;
+
+
+public class GebruikerTest extends TestCase {
+    private final JSONObject database;
+
+    public GebruikerTest(JSONObject database) {
+        this.database = database;
+    }
+
+    @Test
+    public void testGetEmail() throws Exception {
+
+    }
+
+    @Test
+    public void testGetVoornaam() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAchternaam() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWachtwoord() throws Exception {
+
+    }
+
+    @Test
+    public void testGetJSONObject() throws Exception {
+
+    }
+}
