fb41820fd4d1c67d43f43b3a29ba24e48cfedebd
koenvzeijl <kvanzeijl@hotmail.com>
2015-11-25 14:03:17 +0100

    database uitlezen in test

--- src/nl/tudelft/twobrains/server/model/GebruikerTest.java
+++ src/nl/tudelft/twobrains/server/model/GebruikerTest.java
@@ -1,22 +1,27 @@
 package nl.tudelft.twobrains.server.model;
 
 import junit.framework.TestCase;
+import nl.tudelft.twobrains.server.Server;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.junit.Test;
+
+import java.io.IOException;
 
 
 public class GebruikerTest extends TestCase {
     private final JSONObject database;
     private String email;
 
-    public GebruikerTest(JSONObject database) {
-       // Database db = Database.parse("database");
+    public GebruikerTest(JSONObject database) throws IOException, ParseException {
+        Database db = Database.parse(Server.class.getResource("resources/database.json").getFile());
         this.database = database;
+      //  System.out.println(db.toString());
     }
 
     @Test
     public void testGetEmail() throws Exception {
-
+     //   System.out.println(database.toString());
     }
 
     @Test
