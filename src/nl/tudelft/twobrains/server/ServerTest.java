50f9f8454e3e4f5fd365624cda66eb6a946c635a
lvelzel <leroyvelzel@hotmail.com>
2015-11-30 11:42:42 +0100

    ServerTest

---
+++ src/nl/tudelft/twobrains/server/ServerTest.java
@@ -0,0 +1,16 @@
+package nl.tudelft.twobrains.server;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Leroy on 30-11-2015.
+ */
+public class ServerTest {
+
+    @Test
+    public void testieks(){
+        assertTrue(true);
+    }
+}
