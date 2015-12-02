a396b10496a96f18af456977e461a27a606d4502
lvelzel <leroyvelzel@hotmail.com>
2015-12-02 13:50:32 +0100

    Revert "Relocate"
    This reverts commit 30809d29d4d4309aad25299a3d499bcce3531c75.

--- src/nl/tudelft/twobrains/client/model/socket/TwoBrainsSocketTest.java
+++ src/nl/tudelft/twobrains/client/model/socket/TwoBrainsSocketTest.java
@@ -1,6 +1,5 @@
 package nl.tudelft.twobrains.client.model.socket;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,7 +9,7 @@ import static org.junit.Assert.*;
 /**
  * Created by Leroy on 30-11-2015.
  */
-public class TwoBrainsSocketTest extends TestCase {
+public class TwoBrainsSocketTest {
     TwoBrainsSocket testTwoBrainsSocket;
 
     //TODO: Schrijven
--- tests/nl/tudelft/twobrains/client/TwoBrainsTest.java
+++ tests/nl/tudelft/twobrains/client/TwoBrainsTest.java
@@ -1,14 +1,13 @@
 package nl.tudelft.twobrains.client;
 
 import javafx.scene.Scene;
-import junit.framework.TestCase;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Leroy on 30-11-2015.
  */
-public class TwoBrainsTest extends TestCase {
+public class TwoBrainsTest {
 
     TwoBrains testTwoBrains = new TwoBrains();
 
