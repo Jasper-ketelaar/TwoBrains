30809d29d4d4309aad25299a3d499bcce3531c75
Jasper Ketelaar <jasperketelaar@kpnmail.nl>
2015-12-02 13:05:21 +0100

    Relocate

--- tests/nl/tudelft/twobrains/client/TwoBrainsTest.java
+++ tests/nl/tudelft/twobrains/client/TwoBrainsTest.java
@@ -1,13 +1,14 @@
 package nl.tudelft.twobrains.client;
 
 import javafx.scene.Scene;
+import junit.framework.TestCase;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by Leroy on 30-11-2015.
  */
-public class TwoBrainsTest {
+public class TwoBrainsTest extends TestCase {
 
     TwoBrains testTwoBrains = new TwoBrains();
 
--- tests/nl/tudelft/twobrains/client/model/socket/TwoBrainsSocketTest.java
+++ tests/nl/tudelft/twobrains/client/model/socket/TwoBrainsSocketTest.java
@@ -1,5 +1,6 @@
 package nl.tudelft.twobrains.client.model.socket;
 
+import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,7 +10,7 @@ import static org.junit.Assert.*;
 /**
  * Created by Leroy on 30-11-2015.
  */
-public class TwoBrainsSocketTest {
+public class TwoBrainsSocketTest extends TestCase {
     TwoBrainsSocket testTwoBrainsSocket;
 
     //TODO: Schrijven
