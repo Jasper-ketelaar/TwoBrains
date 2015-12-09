9c2edbbca6ef00df5c80e27430d47a8461acf5d5
Jasper Ketelaar <jasperketelaar@kpnmail.nl>
2015-12-09 14:16:03 +0100

    Validation changes

--- src/nl/tudelft/twobrains/client/controller/views/RegisterController.java
+++ src/nl/tudelft/twobrains/client/controller/views/RegisterController.java
@@ -3,25 +3,25 @@ package nl.tudelft.twobrains.client.controller.views;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.*;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import nl.tudelft.twobrains.client.TwoBrains;
 import nl.tudelft.twobrains.client.controller.AbstractController;
+import nl.tudelft.twobrains.client.model.interfaces.Validation;
 import nl.tudelft.twobrains.client.view.popup.TBPopup;
 import nl.tudelft.twobrains.client.view.registreer.comp.TooltipTextField;
 import org.json.simple.JSONObject;
 
 import javax.imageio.ImageIO;
-import javax.tools.Tool;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -29,42 +29,35 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.ResourceBundle;
-import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 /**
  * Created by jasperketelaar on 11/25/15.
  */
-public class RegisterController extends AbstractController {
+public class RegisterController extends AbstractController implements EventHandler<KeyEvent> {
 
     public static final String EMAIL_REGEX = "^[-a-z0-9A-Z~!$%^&*_=+}{'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*@([a-z0-9_][-A-Z-a-z0-9_]*(\\.[-a-z0-9_]+)*\\.(com|edu|net|org|nl)|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,5})?$";
-    final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+    public static final Pattern PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    private final HashMap<String, TooltipTextField> textFields = new HashMap<>();
 
 
     private final TwoBrains twoBrains;
-    @FXML
-    private TextField email;
-    @FXML
-    private PasswordField wachtwoord;
-    @FXML
-    private TextField voornaam;
-    @FXML
-    private TextField achternaam;
+
     @FXML
     private DatePicker geboorte;
-    @FXML
-    private TextField locatie;
-    @FXML
-    private TextField opleiding;
-    @FXML
-    private TextField vakken;
+
     @FXML
     private ImageView profielfoto;
+
     @FXML
     private ComboBox geslacht;
+
     @FXML
     private Button upload;
+
     @FXML
     private Pane gegevens;
 
@@ -106,46 +99,40 @@ public class RegisterController extends AbstractController {
 
     @FXML
     public void register(final ActionEvent evt) {
-        final String emailText = email.getText();
-        final String voornaamText = voornaam.getText();
-        final String achternaamText = achternaam.getText();
+        final String emailText = textFields.get("Email").getTextField().getText();
+        final String voornaamText = textFields.get("Voornaam").getTextField().getText();
+        final String achternaamText = textFields.get("Achternaam").getTextField().getText();
         final String leeftijdText = geboorte.getValue() == null ? "" : calculateAge(geboorte.getValue());
         final String geslachtText = geslacht.getSelectionModel().getSelectedItem().toString();
-        final String wachtwoordText = wachtwoord.getText();
-        final String locatieText = locatie.getText();
-        final String opleidingText = opleiding.getText();
-        final String vakkenText = vakken.getText();
+        final String wachtwoordText = textFields.get("Wachtwoord").getTextField().getText();
+        final String locatieText = textFields.get("Locatie").getTextField().getText();
+        final String opleidingText = textFields.get("Opleiding").getTextField().getText();
+        final String vakkenText = textFields.get("Vakken").getTextField().getText();
         final javafx.scene.image.Image foto = this.profielfoto.getImage();
 
         final boolean fotoVal;
         if (foto == null) {
             upload.getStyleClass().add("error");
-            fotoVal = false;
         } else {
-            fotoVal = true;
             upload.getStyleClass().remove("error");
         }
 
-        if (fotoVal & validate(email, emailText) & validate(voornaam, voornaamText) & validate(achternaam, achternaamText)
-                & validate(geboorte, leeftijdText) & validate(geslacht, geslachtText)
-                & validate(wachtwoord, wachtwoordText) & validate(locatie, locatieText)
-                & validate(opleiding, opleidingText) & validate(vakken, vakkenText)) {
-            final JSONObject user = new JSONObject();
-            user.put("Voornaam", voornaamText);
-            user.put("Achternaam", achternaamText);
-            user.put("Leeftijd", leeftijdText);
-            user.put("Geslacht", geslachtText);
-            user.put("Wachtwoord", wachtwoordText);
-            user.put("Locatie", locatieText);
-            user.put("Opleiding", opleidingText);
-            user.put("Vakken", vakkenText);
 
-            final String response = twoBrains.getSocket().register(emailText, user, this.profileImage);
-            if (response.equals("Succes")) {
-                twoBrains.show(twoBrains.getLoginScene());
-            } else {
+        final JSONObject user = new JSONObject();
+        user.put("Voornaam", voornaamText);
+        user.put("Achternaam", achternaamText);
+        user.put("Leeftijd", leeftijdText);
+        user.put("Geslacht", geslachtText);
+        user.put("Wachtwoord", wachtwoordText);
+        user.put("Locatie", locatieText);
+        user.put("Opleiding", opleidingText);
+        user.put("Vakken", vakkenText);
 
-            }
+        final String response = twoBrains.getSocket().register(emailText, user, this.profileImage);
+        if (response.equals("Succes")) {
+            twoBrains.show(twoBrains.getLoginScene());
+        } else {
+
         }
 
     }
@@ -189,9 +176,52 @@ public class RegisterController extends AbstractController {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        final TooltipTextField email = new TooltipTextField("Test", false);
-        email.getTextField().setPromptText("email@adres.com");
-        gegevens.getChildren().add(new TooltipTextField("Test", false));
+        final TooltipTextField email = new TooltipTextField("Email adres; moet geldig zijn", false, 94.0, 174.0, "email@adres.com");
+        email.setValidation(() -> PATTERN.matcher(email.getTextField().getText()).matches());
+        email.setOnKeyReleased(this);
+        textFields.put("Email", email);
+
+        final TooltipTextField wachtwoord = new TooltipTextField("Wachtwoord; moet minimaal 6 karakters hebben waarvan " +
+                "minimaal 1 hoofdletter en minimaal 1 cijfer", true, 94.0, 214.0, "Wachtwoord");
+        wachtwoord.setValidation(() -> {
+            final String text = wachtwoord.getTextField().getText();
+            if (text.length() < 6) return false;
+
+            boolean upperCase = false;
+            boolean number = false;
+            for (final char c : text.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    upperCase = true;
+                } else if (Character.isDigit(c)) {
+                    number = true;
+                }
+            }
+            return upperCase && number;
+        });
+        wachtwoord.setOnKeyReleased(this);
+        textFields.put("Wachtwoord", wachtwoord);
+
+
+        final TooltipTextField voornaam = new TooltipTextField("Jouw voornaam", false, 94.0, 319, "Voornaam");
+        voornaam.setOnKeyReleased(this);
+        textFields.put("Voornaam", voornaam)
+
+        final TooltipTextField achternaam = new TooltipTextField("Jouw achternaam", false, 94.0, 359, "Achternaam");
+        achternaam.setOnKeyReleased(this);
+
+        final TooltipTextField locatie = new TooltipTextField("Jouw locatie", false, 94.0, 479, "Locatie");
+        locatie.setOnKeyReleased(this);
+
+        final TooltipTextField opleiding = new TooltipTextField("Jouw locatie", false, 94.0, 479, "Locatie");
+        opleiding.setOnKeyReleased(this);
+
+
+        final TooltipTextField opleiding = new TooltipTextField("Wachtwoord; moet minimaal 6 karakters hebben waarvan " +
+                "minimaal 1 hoofdletter en minimaal 1 cijfer", false, 94.0, 519, "Wachtwoord");
+        final TooltipTextField vakken = new TooltipTextField("Wachtwoord; moet minimaal 6 karakters hebben waarvan " +
+                "minimaal 1 hoofdletter en minimaal 1 cijfer", false, 94.0, 559, "Wachtwoord");
+
+        gegevens.getChildren().addAll(email, wachtwoord, voornaam);
     }
 
     @Override
@@ -200,53 +230,22 @@ public class RegisterController extends AbstractController {
         this.geslacht.getItems().addAll("Man", "Vrouw");
     }
 
-
-    @FXML
-    public void emailKeyTyped(final KeyEvent evt) {
-        validate(email, s -> pattern.matcher(s).matches(), email.getText());
-    }
-
-    @FXML
-    public void voornaamKeyPressed(final KeyEvent evt) {
-
-    }
-
-    @FXML
-    public void achternaamKeyPressed(final KeyEvent evt) {
-
-    }
-
-    @FXML
-    public void wachtwoordKeyPressed(final KeyEvent evt) {
-
-    }
-
-
-    @FXML
-    public void locatieKeyPressed(final KeyEvent evt) {
-
-    }
-
-    @FXML
-    public void opleidingKeyPressed(final KeyEvent evt) {
-
-    }
-
-    @FXML
-    public void vakkenKeyPressed(final KeyEvent evt) {
-
-    }
-
-    public <T> void validate(final Node node, final Predicate<T> predicate, final T value) {
-        final ObservableList<String> classes = node.getStyleClass();
-        if (predicate.test(value)) {
-           if(classes.contains("error"))
-               classes.remove("error");
+    public void validate(final TooltipTextField node, final Validation validation) {
+        final ObservableList<String> classes = node.getTextField().getStyleClass();
+        if (validation.validate()) {
+            if (classes.contains("error"))
+                classes.remove("error");
         } else {
-            if(!classes.contains("error"))
+            if (!classes.contains("error"))
                 classes.add("error");
         }
     }
 
 
+    @Override
+    public void handle(KeyEvent event) {
+
+        final TooltipTextField tooltipTextField = (TooltipTextField) event.getSource();
+        validate(tooltipTextField, tooltipTextField.getValidation());
+    }
 }
--- src/nl/tudelft/twobrains/client/view/registreer/comp/TooltipTextField.java
+++ src/nl/tudelft/twobrains/client/view/registreer/comp/TooltipTextField.java
@@ -7,35 +7,52 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import nl.tudelft.twobrains.client.model.interfaces.Validation;
 
 /**
  * Created by jasperketelaar on 12/2/15.
  */
 public class TooltipTextField extends StackPane {
 
+    private Validation validation;
+
     private final Tooltip tooltip;
     private final ImageView imageView;
     private final TextField textField;
 
-    public TooltipTextField(final String tooltip, final boolean password) {
+    public TooltipTextField(final String tooltip, final boolean password, final double x, final double y, final String prompt) {
         this.setPrefSize(285, 30);
+        this.setLayoutX(x);
+        this.setLayoutY(y);
 
         this.textField = password ? new PasswordField() : new TextField();
+        this.textField.setMaxSize(255, 30);
+        this.textField.setPromptText(prompt);
 
         this.tooltip = new Tooltip(tooltip);
         System.out.println(getClass().getResource("../info.png"));
         this.imageView = new ImageView(new Image(getClass().getResource("../info.png").toExternalForm()));
-        this.imageView.setFitHeight(20);
-        this.imageView.setFitWidth(20);
+        this.imageView.setFitHeight(15);
+        this.imageView.setFitWidth(15);
         Tooltip.install(imageView, this.tooltip);
 
         this.getChildren().addAll(textField, imageView);
         StackPane.setAlignment(textField, Pos.CENTER_LEFT);
         StackPane.setAlignment(imageView, Pos.CENTER_RIGHT);
 
+        this.validation = () -> !textField.getText().equals("");
+
     }
 
     public TextField getTextField() {
         return this.textField;
+    }
+
+    public void setValidation(final Validation validation) {
+        this.validation = validation;
+    }
+
+    public Validation getValidation() {
+        return this.validation;
     }
 }
