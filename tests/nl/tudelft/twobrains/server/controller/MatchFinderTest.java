5fca8c2c33ae1e0f5d30630d88984177fcd6534e
lvelzel <leroyvelzel@hotmail.com>
2015-12-16 14:15:55 +0100

    MatchFinderTest
    Uitgebreid

--- tests/nl/tudelft/twobrains/server/controller/MatchFinderTest.java
+++ tests/nl/tudelft/twobrains/server/controller/MatchFinderTest.java
@@ -1,9 +1,14 @@
 package nl.tudelft.twobrains.server.controller;
 
+import nl.tudelft.twobrains.server.Server;
 import nl.tudelft.twobrains.server.model.Database;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,11 +16,31 @@ import static org.junit.Assert.*;
  */
 public class MatchFinderTest {
 
+    Database testDb;
+
+
+
     @Test
     public void testConstructor(){
 
         MatchFinder m = new MatchFinder(new Database(new JSONObject()));
 
         assertNotNull(m);
+    }
+
+    @Test
+    public void testRun(){
+        try {
+            testDb = Database.parse(Server.RESOURCES + "/databasetest.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        MatchFinder m = new MatchFinder(testDb);
+        m.start();
+
+
     }
 }
