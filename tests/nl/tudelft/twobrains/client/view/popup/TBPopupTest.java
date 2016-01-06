3e8b0a051f4d6e678b4ee252d1a757e2845a7fa2
lvelzel <leroyvelzel@hotmail.com>
2016-01-06 13:33:41 +0100

    ChatFXML

--- tests/nl/tudelft/twobrains/client/view/popup/TBPopupTest.java
+++ tests/nl/tudelft/twobrains/client/view/popup/TBPopupTest.java
@@ -1,6 +1,9 @@
 package nl.tudelft.twobrains.client.view.popup;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import nl.tudelft.twobrains.client.view.popup.TBPopup;
 import org.junit.Test;
 
@@ -11,5 +14,14 @@ import org.junit.Test;
 //TODO: weet niet of hier een test voor geschreven moet worden
 public class TBPopupTest {
     Stage testStage = new Stage();
-    TBPopup testTBPopup = new TBPopup(testStage, "Warning", 400,400, 3);
+    TBPopup testTBPopup = new TBPopup(testStage, "Warning", 400, 400, 3);
+
+
+    @Test
+    public void testWarning() {
+
+        TBPopup testTBPopup = new TBPopup(testStage, "Warning", 400, 400, 0);
+        testTBPopup.decodeType(0).equals(Color.RED);
+
+    }
 }
