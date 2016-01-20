1c1350eda4d627ca72e5bb1011d26ba78dab77ce
lvelzel <leroyvelzel@hotmail.com>
2016-01-21 00:24:30 +0100

    Match Test

--- tests/nl/tudelft/twobrains/server/model/MatchTest.java
+++ tests/nl/tudelft/twobrains/server/model/MatchTest.java
@@ -11,18 +11,88 @@ import static org.junit.Assert.*;
 public class MatchTest {
 
     @Test
-    public void testConstructor(){
+    public void testConstructor() {
 
-        final JSONObject user = new JSONObject();
-        user.put("Voornaam", "testLeroy");
-        user.put("Achternaam", "testVelzel");
-        user.put("Leeftijd", "testLeeftijd");
-        user.put("Geslacht", "testGeslacht");
-        user.put("Wachtwoord", "testWachtwoord");
-        user.put("Locatie", "testLocatie");
-        user.put("Opleiding", "testOpleiding");
-        user.put("Vakken", "testVakken");
 
+        final String email = "ibuddyh@gmail.com";
+        final String email2 = "jasperketelaar@kpnmail.nl";
+        final double score = 2.2;
+
+        Match testMatch = new Match(email, email2, score);
+        assertEquals(testMatch.getGebruiker1(), email);
+
+
+    }
+
+    @Test
+    public void testConstructor2() {
+
+
+        final String email = "ibuddyh@gmail.com";
+        final String email2 = "jasperketelaar@kpnmail.nl";
+        final double score = 2.2;
+
+        Match testMatch = new Match(email, email2, score);
+        assertEquals(testMatch.getGebruiker2(), email2);
+
+
+    }
+
+    @Test
+    public void testConstructorScore() {
+
+
+        final String email = "ibuddyh@gmail.com";
+        final String email2 = "jasperketelaar@kpnmail.nl";
+        final double score = 2.2;
+
+        Match testMatch = new Match(email, email2, score);
+        assertTrue(testMatch.getScore() == score);
+
+
+    }
+
+    @Test
+    public void testParse() {
+
+        final String email = "ibuddyh@gmail.com";
+        final String email2 = "jasperketelaar@kpnmail.nl";
+        final double score = 2.2;
+
+        final JSONObject user1 = new JSONObject();
+        user1.put("user1", "ibuddyh@gmail.com");
+        user1.put("user2", "jasperketelaar@kpnmail.nl");
+        user1.put("score", "2.2");
+
+        Match k = Match.parse(user1);
+        Match l = new Match(email, email2, score);
+
+        assertTrue(k.equals(l));
+    }
+
+    @Test
+    public void testEquals() {
+
+        final String email = "ibuddyh@gmail.com";
+        final String email2 = "jasperketelaar@kpnmail.nl";
+        final double score = 2.2;
+
+
+        Match l = new Match(email, email2, score);
+        Match k = new Match(email, email2, score);
+
+        assertTrue(k.equals(l));
+    }
+
+    @Test
+    public void testToString() {
+        final String email = "ibuddyh@gmail.com";
+        final String email2 = "jasperketelaar@kpnmail.nl";
+        final double score = 2.2;
+
+
+        Match l = new Match(email, email2, score);
+        assertEquals(l.toString(), "ibuddyh@gmail.com:jasperketelaar@kpnmail.nl:2.2");
     }
 
 }
